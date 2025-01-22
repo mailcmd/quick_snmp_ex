3cef3406616c9dafb1cd8bf888c607d646315b04
Mauricio Santecchia <mauricios@vianet.com.ar>
2025-01-22 12:11:08 -0300

    .

--- mix.exs
+++ mix.exs
@@ -23,6 +23,8 @@ defmodule QuickSnmpEx.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      { :snmp_ex, git: "https://github.com/mailcmd/snmp-elixir.git" },
+      { :log_ex, git: "https://github.com/mailcmd/log_ex.git"}
     ]
   end
 end
