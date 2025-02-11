abd146b15f4c76a73b31ba26c3c78a96f8c076b9
Mauricio Santecchia <mauricios@vianet.com.ar>
2025-02-11 17:34:00 -0300

    .

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule QuickSnmpEx.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, QuickSnmp ]
+      extra_applications: [:logger, :quick_snmp ]
     ]
   end
 
